d1689b5d8dec63c4086dac6e9a3ef390701649cd
charmingruby <mrrubycanal3105@gmail.com>
2023-02-15 21:11:01 -0300

    squads using async storage

--- mobile/src/screens/Groups/index.tsx
+++ mobile/src/screens/Groups/index.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
-
-import { useNavigation } from '@react-navigation/native';
-
+import { useState, useCallback } from 'react';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 import { FlatList } from 'react-native';
 
 import * as S from './styles';
@@ -11,14 +9,41 @@ import { GroupCard } from '@components/GroupCard';
 import { EmptyList } from '@components/EmptyList';
 import { Button } from '@components/Button';
 
+import { groupGetAll } from '@storage/group/groupGetAll';
+import { groupCreate } from '@storage/group/groupCreate';
+
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Study Group', 'Work']);
+  const [groups, setGroups] = useState<string[]>([]);
 
   const navigation = useNavigation();
 
-  function handleNewGroup() {
-    navigation.navigate('new');
+  async function handleNewGroup() {
+    try {
+      navigation.navigate('new');
+    } catch (error) {
+      throw error;
+    }
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupGetAll();
+
+      setGroups(data);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group });
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
 
   return (
     <S.Container>
@@ -28,9 +53,15 @@ export function Groups() {
       <FlatList
         data={groups}
         keyExtractor={(item) => item}
-        renderItem={({ item }) => <GroupCard title={item} />}
+        renderItem={({ item }) => (
+          <GroupCard title={item} onPress={() => handleOpenGroup(item)} />
+        )}
         ListEmptyComponent={() => <EmptyList message="Empty Squad List." />}
         showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          groups.length === 0 && { flex: 1 },
+        ]}
       />
 
       <Button title="Create new squad" onPress={handleNewGroup} />
--- mobile/src/screens/NewGroups/index.tsx
+++ mobile/src/screens/NewGroups/index.tsx
@@ -6,6 +6,10 @@ import { Input } from '@components/Input';
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
 
+import { groupCreate } from '@storage/group/groupCreate';
+import { AppError } from '@utils/AppError';
+import { Alert } from 'react-native';
+
 import * as S from './styles';
 
 export function NewGroups() {
@@ -13,8 +17,21 @@ export function NewGroups() {
 
   const navigation = useNavigation();
 
-  function handleNew() {
-    navigation.navigate('players', { group: `${group}` });
+  async function handleNew() {
+    try {
+      if (group.trim().length === 0) {
+        return Alert.alert('New Squad', 'Type the name of the squad.');
+      }
+
+      await groupCreate(group);
+      navigation.navigate('players', { group: `${group}` });
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('New Squad', error.message);
+      } else {
+        Alert.alert('New Squad', 'Unable to create squad.');
+      }
+    }
   }
 
   return (
